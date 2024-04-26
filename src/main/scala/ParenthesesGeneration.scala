89e220eb9d8130716031822b262efa63a8ff19c3
KUSHAGRA <83748861+Kushagra4679@users.noreply.github.com>
2024-04-26 16:16:34 +0530

    Update ParenthesesGeneration.scala

--- src/main/scala/ParenthesesGeneration.scala
+++ src/main/scala/ParenthesesGeneration.scala
@@ -4,12 +4,12 @@ class ParenthesesGeneration {
     val validParentheses = new ListBuffer[String]
 
     def parentheseHelper(left: Int, right: Int, s: String): Unit = {
-      if (left == 0 && right == 0) validParentheses.append(s)
-      if (left > 0) parentheseHelper(left - 1, right, s + "(")
-      if (left < right) parentheseHelper(left, right - 1, s + ")")
+      if (s.size == 2*n ) validParentheses.append(s)
+      if (left < n) parentheseHelper(left + 1, right, s + "(")
+      if (left > right && right <= n) parentheseHelper(left, right + 1, s + ")")
     }
 
-    parentheseHelper(n, n, "")
+    parentheseHelper(0, 0, "")
     validParentheses.toList
   }
 }
